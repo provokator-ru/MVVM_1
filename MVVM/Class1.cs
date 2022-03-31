93cb35979fb46fe7c110bc907ce74dcb6f0d644e
Dmitry <dimasokolov2002g@gmail.com>
2022-04-01 01:12:37 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    static class MathFuncs
+    {
+        public static int GetSumOf(int a, int b) => a + b;
+    }
+}
